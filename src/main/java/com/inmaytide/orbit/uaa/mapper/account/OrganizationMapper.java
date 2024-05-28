c9d7e86582adb3397982a563a3155b6d8797bb8f
inmaytide <inmaytide@gmail.com>
2024-05-28 14:38:49 +0800

    :construction: 组织管理&&岗位管理.2

--- src/main/java/com/inmaytide/orbit/uaa/mapper/account/OrganizationMapper.java
+++ src/main/java/com/inmaytide/orbit/uaa/mapper/account/OrganizationMapper.java
@@ -2,10 +2,12 @@ package com.inmaytide.orbit.uaa.mapper.account;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.inmaytide.orbit.uaa.domain.account.Organization;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author inmaytide
  * @since 2024/5/28
  */
+@Mapper
 public interface OrganizationMapper extends BaseMapper<Organization> {
 }
--- src/main/java/com/inmaytide/orbit/uaa/mapper/account/PositionMapper.java
+++ src/main/java/com/inmaytide/orbit/uaa/mapper/account/PositionMapper.java
@@ -2,10 +2,12 @@ package com.inmaytide.orbit.uaa.mapper.account;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.inmaytide.orbit.uaa.domain.account.Position;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author inmaytide
  * @since 2024/5/28
  */
+@Mapper
 public interface PositionMapper extends BaseMapper<Position> {
 }
