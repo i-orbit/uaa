49ecdd58e35d3b1ba381ed63bc136b8dec0a7a0d
inmaytide <inmaytide@gmail.com>
2024-07-12 13:28:34 +0800

    :construction: bug fixes

--- src/main/java/com/inmaytide/orbit/uaa/mapper/permission/AreaMapper.java
+++ src/main/java/com/inmaytide/orbit/uaa/mapper/permission/AreaMapper.java
@@ -2,10 +2,12 @@ package com.inmaytide.orbit.uaa.mapper.permission;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.inmaytide.orbit.uaa.domain.permission.Area;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author inmaytide
  * @since 2024/7/12
  */
+@Mapper
 public interface AreaMapper extends BaseMapper<Area> {
 }
